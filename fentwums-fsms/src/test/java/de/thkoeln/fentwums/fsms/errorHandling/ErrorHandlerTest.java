36c988a5a864f6dfe367c71282690ad065a31e1f
tunckoldas <tunc.koldas1@smail.th-koeln.de>
2026-01-28 15:08:59 +0100

    refactor

--- fentwums-fsms/src/test/java/de/thkoeln/fentwums/fsms/errorHandling/ErrorHandlerTest.java
+++ fentwums-fsms/src/test/java/de/thkoeln/fentwums/fsms/errorHandling/ErrorHandlerTest.java
@@ -5,7 +5,7 @@
  */
 package de.thkoeln.fentwums.fsms.errorHandling;
 
-import fsms.logHandling.LogHandler;
+import de.thkoeln.fentwums.fsms.logHandling.LogHandler;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
